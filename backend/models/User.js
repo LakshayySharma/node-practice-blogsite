d69ff58ef4bce2677be31d75f54e20fd12f24b14
lucky <ls199575@gmail.com>
2021-03-14 22:29:01 +0530

    User model added

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+});
+
+module.exports = User = mongoose.model("User", userSchema);
