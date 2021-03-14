de465e2b98bed5da173dd90b20afc54858f0e9ed
lucky <ls199575@gmail.com>
2021-03-14 22:30:59 +0530

    add signup and all users route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const userController = require("../controllers/userController");
+require("dotenv").config();
 
-router.get("/", (req, res, next) => {
-  res.send("<h1>Welcome!</h1>");
-});
-
+router.get("/", userController.getAllUser);
+router.post("/", userController.signUpUser);
 module.exports = router;
